2443655466654ad0285775fe8110343bb963a724
Lyle Mitchell <ljwmitchell@gmail.com>
2018-06-23 19:54:03 +0100

    adds intial commit plus title and author information

---
+++ Reading_List_2018.py
@@ -0,0 +1,64 @@
+# books_2018 = [
+# {
+# Title: "Sapiens: A Brief History of Humankind",
+# Author: "Yuval Noah Harari"
+# },
+# {
+# Title: "Difficult Conversations",
+# Author: "Bruce Patton, Douglas Stone & Sheila Heen"
+# },
+# {
+# Title: "Creative Sparks",
+# Author: "Jim Krause"
+# },
+# Title: "Dune",
+# Author: "Frank Herbert"
+# },
+# Title: "12 Rules for Life: An Antidote to Chaos",
+# Author: "Jordan B Peterson"
+# },
+# Title: "The Pragmatic Programmer",
+# Author: "Andrew Hunt & David Thomas"
+# },
+# Title: "Learn to Program",
+# Author: "Chris Pine"
+# },
+# Title: "Home Deus: A Brief History of Tomorrow",
+# Author: "Yuval Noah Harari"
+# },
+# }
+# Title: "The Mandibles: A Family, 2029-2047",
+# Author: "Lionel Shriver"
+# },
+# }
+# Title: "The Call of Cthulhu and Other Weird Stories",
+# Author: "H.P. Lovecraft"
+# }
+# ]
+
+# print books_2018
+
+books_2018 = {
+"Sapiens: A Brief History of Humankind" : "Yuval Noah Harari",
+
+"Difficult Conversations" : "Bruce Patton, Douglas Stone & Sheila Heen",
+
+"Creative Sparks" : "Jim Krause",
+
+"Dune" : "Frank Herbert",
+
+"12 Rules for Life: An Antidote to Chaos" : "Jordan B Peterson",
+
+"The Pragmatic Programmer" : "Andrew Hunt & David Thomas",
+
+"Learn to Program" : "Chris Pine",
+
+"Home Deus: A Brief History of Tomorrow" : "Yuval Noah Harari",
+
+"The Mandibles: A Family, 2029-2047": "Lionel Shriver",
+
+"The Call of Cthulhu and Other Weird Stories" : "H.P. Lovecraft",
+}
+
+# print (books_2018)
+print (len(books_2018))
